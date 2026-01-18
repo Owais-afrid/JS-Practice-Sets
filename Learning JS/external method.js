b28b8b294c51c35059d51661cafab6fe1535da35
Owais-afrid <owaisafrid29@gmail.com>
2026-01-18 11:13:06 +0530

    JS Practice Files which i used for Practice

---
+++ Learning JS/external method.js
@@ -0,0 +1,4 @@
+alert ('Yoo zevv how you been')
+console.log ('This is to report that this is typing in the console')
+
+// here i understood how to use the external method to link html file with js file.
